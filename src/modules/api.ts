ef7251bb7d2f19c682d61123e22489955f3247ce
ryohidaka <git@ryohidaka.jp>
2023-02-10 18:26:47 +0900

    feat: API共通リクエスト処理を追加

--- src/modules/api.ts
+++ src/modules/api.ts
@@ -1,4 +1,5 @@
-import { NextApiResponse } from "next";
+import { getCookie } from "cookies-next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 /**
  * 共通のエラーを返却する
@@ -15,4 +16,40 @@ export const returnErrorResponse = (
       error: error,
     })
   );
+};
+
+/**
+ * Misskey API 共通リクエスト
+ * @param path
+ * @param req
+ * @param res
+ * @returns
+ */
+export const apiRequest = async (
+  path: string,
+  req: NextApiRequest,
+  res: NextApiResponse,
+  params?: object
+) => {
+  const host = getCookie("host", { req, res });
+  const token = getCookie("token", { req, res });
+
+  const url = `${host}${path}`;
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  const baseRaw = { i: token };
+  const raw = JSON.stringify({ ...baseRaw, ...params });
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: raw,
+    headers: headers,
+    redirect: "follow",
+  });
+
+  const data = await response.json();
+
+  return data;
 };

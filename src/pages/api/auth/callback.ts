bae006b2c11e5934230743cbf02b602e5752fd4c
ryohidaka <git@ryohidaka.jp>
2023-02-10 18:19:02 +0900

    feat: MiAuthによる認証を追加

---
+++ src/pages/api/auth/callback.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { returnErrorResponse } from "@/modules/api";
+import { getCookie, setCookie } from "cookies-next";
+
+type Result = {
+  ok: boolean;
+  token?: string;
+};
+
+/**
+ * コールバック
+ */
+const callback = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const session = req.query["session"] as string;
+    const host = getCookie("host", { req, res });
+    const url = `${host}/api/miauth/${session}/check`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      redirect: "follow",
+    });
+
+    const data: Result = await response.json();
+
+    if (!data.ok) {
+      throw new Error("false");
+    }
+
+    setCookie("token", data.token, { req, res, maxAge: 60 * 60 * 24 });
+
+    res.redirect("/");
+  } catch (error) {
+    returnErrorResponse(res, error);
+  }
+};
+
+export default callback;

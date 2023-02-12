f6c7b46f6fe78e94e4bd5cd5443854e40791294b
ryohidaka <git@ryohidaka.jp>
2023-02-12 23:40:35 +0900

    fix: 投稿者表示を修正

--- src/components/Posts/Author.tsx
+++ src/components/Posts/Author.tsx
@@ -11,18 +11,20 @@ type Props = {
  */
 export const Author = ({ user }: Props) => {
   return (
-    <div className="flex items-center gap-3">
-      <Image
-        width={40}
-        height={40}
-        alt={user?.name}
-        src={user?.image_url}
-        unoptimized
-        className="aspect-square rounded-full"
-      />
-      <p className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-        {user?.name}
-      </p>
+    <div className="sticky top-12 z-50 p-5">
+      <div className="flex items-center gap-3">
+        <Image
+          width={40}
+          height={40}
+          alt={user?.name}
+          src={user?.image_url}
+          unoptimized
+          className="aspect-square rounded-full"
+        />
+        <p className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {user?.name}
+        </p>
+      </div>
     </div>
   );
 };

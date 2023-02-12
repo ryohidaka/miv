6d5caf256f2c9d7f9b2890a4cb237023bd547c4a
ryohidaka <git@ryohidaka.jp>
2023-02-12 23:50:03 +0900

    fix: ギャラリー個別ページのレイアウト修正

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { CommonImage } from "@/components/Common/Image";
 import { ViewerLayout } from "@/components/Viewer/Layout";
 import { User } from "@/types/user";
 import { Author } from "@/components/Posts/Author";
 import { useGalleryPost } from "@/hooks/gallery/post";
+import { Content } from "@/components/Posts/Content";
 
 type Props = {
   params: {
@@ -28,17 +28,11 @@ export default function ShowPostImages({ params }: Props) {
   // 投稿を取得
   const { data, error, isLoading } = useGalleryPost(postId);
 
-  const images = data?.images;
-
   return (
     <ViewerLayout isLoading={isLoading} error={error}>
       <div className="flex flex-wrap">
         <article className="grid w-full grid-cols-1 md:w-3/4">
-          {images?.map((image) => {
-            return <CommonImage key={image.id} image={image} />;
-          })}
-          <h1>{data?.title}</h1>
-          <p>{data?.description}</p>
+          {data && <Content post={data} />}
         </article>
         <aside className="w-full md:w-1/4">
           <Author user={data?.user as User} />
---
+++ src/components/Posts/Content.tsx
@@ -0,0 +1,65 @@
+import { Post } from "@/types/post";
+import { useState } from "react";
+import { CommonImage } from "../Common/Image";
+import { NumberBadge } from "../TileList/Tile/NumberBadge";
+
+type Props = {
+  post: Post;
+};
+
+/**
+ * コンテンツ
+ * @returns
+ */
+export const Content = ({ post }: Props) => {
+  const images = post.images;
+
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  const topImage = images[0];
+
+  return (
+    <>
+      <div className="overflow-hidden rounded-lg bg-white shadow dark:bg-gray-800">
+        {!isOpen ? (
+          <div className="relative grid place-items-center">
+            <CommonImage
+              key={topImage.id}
+              image={topImage}
+              className="aspect-video object-cover"
+            />
+            <NumberBadge count={images.length} />
+
+            <button
+              className="light-btn absolute bottom-3 z-40 mx-auto !rounded-full"
+              onClick={() => setOpen(true)}
+            >
+              すべて見る
+            </button>
+          </div>
+        ) : (
+          <div className="relative grid grid-cols-1 gap-1 md:gap-10">
+            {images.map((image) => {
+              return (
+                <CommonImage
+                  key={image.id}
+                  image={image}
+                  className="!h-auto !w-auto"
+                />
+              );
+            })}
+          </div>
+        )}
+        <div className="p-5">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {post?.title}
+          </h5>
+
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {post?.description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};

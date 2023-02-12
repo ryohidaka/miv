08e125cf32ae1b648b6961384eda0cdbe4638cf6
ryohidaka <git@ryohidaka.jp>
2023-02-12 21:09:28 +0900

    feat: ギャラリーの型定義を追加

---
+++ src/types/gallery.ts
@@ -0,0 +1,16 @@
+import { DriveFile, User } from "misskey-js/built/entities";
+
+export type GalleryPost = {
+  id: string;
+  user: User;
+  title: string;
+  description: string | null;
+  files: DriveFile[];
+  likedCount: number;
+  isLiked?: boolean;
+};
+
+export type GalleryParams = {
+  limit: number;
+  untilId?: string;
+};

efe7d1f1a075c7e4190a9b2f0355dc8ca1917f20
ryohidaka <git@ryohidaka.jp>
2023-02-13 10:13:53 +0900

    feat: ギャラリーページのURLを変更

--- src/components/Header/Sidebar/SideMenu/index.tsx
+++ src/components/Header/Sidebar/SideMenu/index.tsx
@@ -13,30 +13,40 @@ type Props = {
  */
 export default function SideMenu({ onClose }: Props) {
   // ページ一覧
-  const pages = [
+  const links = [
     {
-      url: "/",
       label: "Gallery",
+      pages: [
+        {
+          url: "/",
+          label: "Posts",
+        },
+        {
+          url: "gallery/featured",
+          label: "Featured",
+        },
+        {
+          url: "gallery/popular",
+          label: "Popular",
+        },
+      ],
     },
     {
-      url: "/featured",
-      label: "Gallery (Featured)",
-    },
-    {
-      url: "/popular",
-      label: "Gallery (Popular)",
-    },
-    {
-      url: "/home",
-      label: "Home Timeline",
-    },
-    {
-      url: "/user",
-      label: "User Timeline",
-    },
-    {
-      url: "/search",
-      label: "Search Timeline",
+      label: "Timeline",
+      pages: [
+        {
+          url: "/home",
+          label: "Home",
+        },
+        {
+          url: "/user",
+          label: "User",
+        },
+        {
+          url: "/search",
+          label: "Search",
+        },
+      ],
     },
   ];
 
@@ -45,16 +55,20 @@ export default function SideMenu({ onClose }: Props) {
 
   return (
     <nav className="mb-5 grid grid-cols-1 items-stretch justify-center gap-3">
-      <h3 className="text-base font-semibold">Links</h3>
+      {links.map((link) => (
+        <>
+          <h3 className="text-base font-semibold">{link.label}</h3>
 
-      {pages.map((page) => (
-        <MenuLink
-          key={page.label}
-          url={page.url}
-          label={page.label}
-          isActive={path === page.url}
-          onClick={onClose}
-        />
+          {link.pages.map((page) => (
+            <MenuLink
+              key={page.label}
+              url={page.url}
+              label={page.label}
+              isActive={path === page.url}
+              onClick={onClose}
+            />
+          ))}
+        </>
       ))}
     </nav>
   );

f318eead3e2ac8ae52f8b0f59a85ca67ff916e31
ryohidaka <git@ryohidaka.jp>
2023-02-13 22:29:59 +0900

    fix: 不要なコンポーネントを削除

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { ViewerLayout } from "@/components/Viewer/Layout";
 import { useKeyword, useSearchTimeline } from "@/hooks/timeline/search";
-import { SearchForm } from "@/components/Viewer/Layout/SearchForm";
 import { NoResults } from "@/components/Viewer/NoResults";
-import { useSearchHistory } from "@/hooks/history";
 import { Viewer } from "@/components/Viewer";
 
 /**
@@ -23,23 +20,10 @@ export default function Search() {
     setSize(size + 1);
   };
 
-  const { updateHistory } = useSearchHistory();
-
-  const router = useRouter();
-
-  // 検索
-  const search = (keyword: string) => {
-    updateHistory(keyword);
-    router.push(`/search?keyword=${keyword}`);
-  };
-
   const noResult = keyword && data.length === 0;
 
   return (
     <ViewerLayout isLoading={isLoading} error={error}>
-      {/* 検索フォーム */}
-      <SearchForm value={keyword} search={search} />
-
       {noResult && <NoResults keyword={keyword} />}
 
       <Viewer posts={data} hasMore={true} next={next} />

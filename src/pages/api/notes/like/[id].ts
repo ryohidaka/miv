563212ec5a16f09b30a77d7369ffc74ea117c63e
ryohidaka <git@ryohidaka.jp>
2023-02-13 17:51:42 +0900

    feat: ノートのいいね登録処理を追加

---
+++ src/pages/api/notes/like/[id].ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { apiRequest, returnErrorResponse } from "@/modules/api";
+
+/**
+ * いいね登録
+ */
+const like = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const url = "/api/notes/favorites/create";
+
+    const params = {
+      noteId: id,
+    };
+
+    await apiRequest(url, req, res, params);
+
+    res.status(200);
+  } catch (error) {
+    returnErrorResponse(res, error);
+  }
+};
+
+export default like;

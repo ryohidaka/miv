2832d9a9123833b986893da9b90576bd38d3d620
ryohidaka <git@ryohidaka.jp>
2023-02-13 14:15:35 +0900

    fix: タイル表示のカラム数を修正

--- src/components/TileList/index.tsx
+++ src/components/TileList/index.tsx
@@ -12,7 +12,7 @@ type Props = {
  */
 export const TileList = ({ posts, isGallery }: Props) => {
   return (
-    <div className="grid sm:grid-cols-2 sm:gap-x-0.5 sm:gap-y-0.5 md:grid-cols-3 md:gap-x-1 md:gap-y-1 lg:grid-cols-6 lg:gap-x-6 lg:gap-y-6">
+    <div className="grid grid-cols-2 gap-0.5 md:grid-cols-3 md:gap-x-1 md:gap-y-1 lg:grid-cols-6 lg:gap-x-6 lg:gap-y-6">
       {posts?.map((post) => (
         <Tile key={post.id} post={post} isGallery={isGallery} />
       ))}

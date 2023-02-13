8d910310ce477a63286992d1c7a37da2cf415bd4
ryohidaka <git@ryohidaka.jp>
2023-02-13 22:19:20 +0900

    fix: ドロワーをTailwindに置き換え

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { usePathname } from "next/navigation";
 import { capitalize } from "@/modules/client";
 
 import Sidebar from "./Sidebar";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 /**
  * 共通ヘッダー
@@ -15,13 +17,28 @@ export default function Header() {
   const pagePath = path?.replace("/", "") as string;
   const pageName = capitalize(pagePath);
 
+  const [isOpen, setOpen] = useState(false);
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between bg-white px-3 py-2 dark:bg-gray-800">
-      {/* アプリ名 */}
-      <h1 className="text-xl">{pageName}</h1>
+    <>
+      <header className="sticky top-0 z-50 flex items-center justify-between bg-white px-3 py-2 dark:bg-gray-800">
+        {/* アプリ名 */}
+        <h1 className="text-xl">{pageName}</h1>
+
+        {!isOpen && (
+          <AiOutlineMenu
+            className="fill-white text-2xl mix-blend-difference"
+            onClick={() => setOpen(true)}
+          />
+        )}
+      </header>
 
       {/* サイドバー */}
-      <Sidebar />
-    </header>
+      <Sidebar isOpen={isOpen} onClose={onClose} />
+    </>
   );
 }

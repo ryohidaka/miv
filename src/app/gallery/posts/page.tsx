e606b162170ba6f0c5721a54e356baa619f310d3
ryohidaka <git@ryohidaka.jp>
2023-02-13 12:26:50 +0900

    feat: ギャラリー一覧画面を追加

---
+++ src/app/gallery/posts/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Viewer } from "@/components/Viewer";
+import { ViewerLayout } from "@/components/Viewer/Layout";
+import { useGalleryPosts } from "@/hooks/gallery/posts";
+
+/**
+ * ギャラリー画面
+ * @returns
+ */
+export default function GalleryPosts() {
+  const { data, error, isLoading, size, setSize } = useGalleryPosts();
+
+  const next = () => {
+    setSize(size + 1);
+  };
+
+  return (
+    <ViewerLayout isLoading={isLoading} error={error}>
+      {/* @ts-ignore */}
+      <Viewer posts={data} hasMore={true} next={next} isGallery />
+    </ViewerLayout>
+  );
+}

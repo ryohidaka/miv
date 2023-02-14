f2a55ca5cefa03e91a093226ed8afd562d14f0ad
ryohidaka <git@ryohidaka.jp>
2023-02-14 23:40:01 +0900

    remove: 不要な処理を削除

--- src/modules/client.ts
+++ src/modules/client.ts
@@ -1,18 +1,4 @@
-import { Posts } from "@/types/post";
 import { DriveFile } from "misskey-js/built/entities";
-
-/**
- * 投稿の配列を平坦化して返却する
- * @param data
- * @returns
- */
-export const getFlatPosts = (data: Posts[]): Posts => {
-  const flatPosts = data
-    ? data.filter((data) => typeof data !== "undefined").flat()
-    : [];
-
-  return flatPosts;
-};
 
 /**
  * 先頭のみ大文字にして返却する

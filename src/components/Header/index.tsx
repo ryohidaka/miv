448b9b3575b7b77621cb45b2ac81eab579987118
ryohidaka <git@ryohidaka.jp>
2023-02-14 20:39:12 +0900

    feat: 検索フォームを追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { APP_NAME } from "@/modules/const";
 import Instance from "./Instance";
+import SearchInput from "./SearchInput";
 
 /**
  * 共通ヘッダー
@@ -17,7 +18,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="sticky top-0 z-50 flex items-center justify-between bg-white px-3 py-2 dark:bg-neutral-900">
+      <header className="sticky top-0 z-50 flex items-center bg-white px-3 py-2 dark:bg-neutral-900">
         {/* ハンバーガーボタン */}
         <AiOutlineMenu
           className="absolute fill-white text-2xl mix-blend-difference"
@@ -25,12 +26,15 @@ export default function Header() {
         />
 
         {/* アプリ名 */}
-        <h1 className="mx-auto flex items-center gap-1 text-xl">
+        <h1 className="mx-auto flex w-1/4 items-center gap-1 text-xl md:mx-10">
           {APP_NAME}
 
           {/* インスタンス名 */}
           <Instance />
         </h1>
+
+        {/* 検索フォーム */}
+        <SearchInput />
       </header>
 
       {/* サイドバー */}

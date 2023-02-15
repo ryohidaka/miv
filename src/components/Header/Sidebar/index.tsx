dfaa449e5ab88d69c81ae1cbb4c884dc148a4a81
ryohidaka <git@ryohidaka.jp>
2023-02-15 23:41:35 +0900

    refactor: ドロワーを別コンポーネントに分離

--- src/components/Header/Sidebar/index.tsx
+++ src/components/Header/Sidebar/index.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import SideMenu from "./SideMenu";
-import { AiOutlineClose } from "react-icons/ai";
 import { BiLinkExternal } from "react-icons/bi";
 import Link from "next/link";
 import { useRecoilState } from "recoil";
 import { newTabState } from "@/atoms/NewTab";
-import Drawer from "react-modern-drawer";
 import { deleteCookie } from "cookies-next";
+import CommonDrawer from "@/components/Common/Drawer";
 
 type Props = {
   isOpen: boolean;
@@ -30,63 +29,45 @@ export default function Sidebar({ isOpen, onClose }: Props) {
   };
 
   return (
-    <Drawer
-      customIdSuffix="drawer"
-      open={isOpen}
+    <CommonDrawer
+      isOpen={isOpen}
       onClose={onClose}
       direction="left"
-      className="h-screen w-80 overflow-y-scroll bg-white p-4 dark:!bg-gray-800"
-      lockBackgroundScroll={true}
+      className="overflow-y-scroll"
+      title="Menu"
     >
-      <h5
-        id="drawer-navigation-label"
-        className="text-base font-semibold uppercase text-gray-500 dark:text-gray-400"
-      >
-        Menu
-      </h5>
-      <button
-        type="button"
-        className="absolute top-2.5 right-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-      >
-        <AiOutlineClose onClick={onClose} />
-        <span className="sr-only">Close menu</span>
-      </button>
-      <div className="overflow-y-auto py-4">
-        <div className="grid grid-cols-1 gap-3">
-          {/* ページリンク */}
-          <SideMenu onClose={onClose} />
+      {/* ページリンク */}
+      <SideMenu onClose={onClose} />
 
-          {/* 別タブ遷移設定 */}
-          <div className="mb-4 flex items-center">
-            <input
-              id="default-checkbox"
-              type="checkbox"
-              value=""
-              checked={newTab}
-              onChange={(e) => setNewTab(e.target.checked)}
-              className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
-            />
-            <label
-              htmlFor="default-checkbox"
-              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Open in new tab
-            </label>
-          </div>
-
-          {/* ログアウト */}
-          <button className="btn-outline" onClick={logout}>
-            ログアウト
-          </button>
-
-          {/* Copyrights */}
-          <div className="flex items-center gap-1">
-            &copy;
-            <Link href="https://ryohidaka.jp">ryohidaka</Link>
-            <BiLinkExternal />
-          </div>
-        </div>
+      {/* 別タブ遷移設定 */}
+      <div className="mb-4 flex items-center">
+        <input
+          id="default-checkbox"
+          type="checkbox"
+          value=""
+          checked={newTab}
+          onChange={(e) => setNewTab(e.target.checked)}
+          className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+        />
+        <label
+          htmlFor="default-checkbox"
+          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        >
+          Open in new tab
+        </label>
       </div>
-    </Drawer>
+
+      {/* ログアウト */}
+      <button className="btn-outline w-full" onClick={logout}>
+        ログアウト
+      </button>
+
+      {/* Copyrights */}
+      <div className="flex items-center gap-1">
+        &copy;
+        <Link href="https://ryohidaka.jp">ryohidaka</Link>
+        <BiLinkExternal />
+      </div>
+    </CommonDrawer>
   );
 }

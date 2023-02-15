bed8a142eb3984bc88c1eea1d08696ba6846369d
ryohidaka <git@ryohidaka.jp>
2023-02-15 19:45:59 +0900

    fix: ハッシュタグ一覧取得処理を修正

--- src/pages/api/tags/trend.ts
+++ src/pages/api/tags/trend.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { apiRequest, returnErrorResponse } from "@/modules/api";
+import { Hashtag } from "@/types/tag";
 
 /**
  * トレンドにあるハッシュタグを取得
@@ -10,7 +11,9 @@ const getNotesByTag = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const url = "/hashtags/trend";
 
-    const tags = await apiRequest(url, req, res);
+    const data = await apiRequest(url, req, res);
+
+    const tags: string[] = data?.map((trend: Hashtag) => trend.tag);
 
     res.status(200).json(tags);
   } catch (error) {

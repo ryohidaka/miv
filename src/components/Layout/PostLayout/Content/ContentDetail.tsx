d0f44a24d6a0ce0562693d67422af16d3698311d
ryohidaka <git@ryohidaka.jp>
2023-02-15 11:56:36 +0900

    fix: 詳細表示のスタイル修正

--- src/components/Layout/PostLayout/Content/ContentDetail.tsx
+++ src/components/Layout/PostLayout/Content/ContentDetail.tsx
@@ -12,14 +12,14 @@ type Props = {
  */
 export const ContentDetail = ({ title, description, tags }: Props) => {
   return (
-    <div className="p-5">
+    <div className="grid grid-cols-1 gap-5 p-5">
       {/* タイトル */}
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
       </h5>
 
       {/* 説明文 */}
-      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+      <p className="font-normal text-gray-700 dark:text-gray-400">
         {description}
       </p>
 

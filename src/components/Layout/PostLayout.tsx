0df94dfb3ef7e80b871927807e7be065681863d6
ryohidaka <git@ryohidaka.jp>
2023-02-15 11:38:21 +0900

    refactor: 投稿個別ページのレイアウトを共通化

---
+++ src/components/Layout/PostLayout.tsx
@@ -0,0 +1,30 @@
+import { Post } from "@/types/post";
+import { Author } from "../Common/Author";
+import { StickyTop } from "../Common/StickyTop";
+import { Content } from "../Posts/Content";
+
+type Props = {
+  post: Post;
+};
+
+/**
+ * 投稿個別ページレイアウト
+ * @returns
+ */
+export const PostLayout = ({ post }: Props) => {
+  return (
+    <div className="flex flex-wrap">
+      {/* メインカラム */}
+      <article className="grid w-full grid-cols-1 md:w-3/4 md:py-6">
+        <Content post={post} />
+      </article>
+
+      {/* サイドカラム */}
+      <aside className="w-full p-5 md:w-1/4">
+        <StickyTop>
+          <Author user={post.user} imageSize={45} />
+        </StickyTop>
+      </aside>
+    </div>
+  );
+};

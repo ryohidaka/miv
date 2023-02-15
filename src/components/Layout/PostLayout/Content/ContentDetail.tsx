0db6a988b097d590dcfeae071d387230b6b8138b
ryohidaka <git@ryohidaka.jp>
2023-02-15 11:47:11 +0900

    refactor: コンテンツ詳細表示を別コンポーネントに分離

---
+++ src/components/Layout/PostLayout/Content/ContentDetail.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+type Props = {
+  title?: string;
+  description?: string;
+  tags?: string[];
+};
+
+/**
+ * コンテンツ詳細表示
+ * @returns
+ */
+export const ContentDetail = ({ title, description, tags }: Props) => {
+  return (
+    <div className="p-5">
+      {/* タイトル */}
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+
+      {/* 説明文 */}
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+
+      {/* ハッシュタグ */}
+      <ul className="flex flex-wrap gap-3">
+        {tags?.map((tag) => (
+          <li key={tag}>
+            <Link href={`/tags/${tag}`} className="hyperlink">
+              {tag}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};

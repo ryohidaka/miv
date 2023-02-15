b501bc15fbc73e7c93a1845f9ae592330aa2d3f7
ryohidaka <git@ryohidaka.jp>
2023-02-16 01:23:41 +0900

    fix: 画像のスケルトン表示を共通画像コンポーネントに統合

--- src/components/Common/Image.tsx
+++ src/components/Common/Image.tsx
@@ -1,6 +1,7 @@
 import { Image as ImageType } from "@/types/image";
 import classNames from "classnames";
 import Image from "next/image";
+import { BsImageAlt } from "react-icons/bs";
 
 type Props = {
   image: ImageType;
@@ -15,19 +16,32 @@ type Props = {
 export const CommonImage = ({ image, className, thumbnail }: Props) => {
   const src = thumbnail ? image.thumbnailUrl || image.url : image.url;
   return (
-    <Image
-      className={classNames(
-        "!relative block h-full w-full object-contain",
-        className
+    <>
+      {src ? (
+        <Image
+          className={classNames(
+            "!relative block h-full w-full object-contain",
+            className
+          )}
+          key={image.id}
+          src={src}
+          alt={image.id}
+          fill
+          quality={100}
+          unoptimized
+          placeholder={image.blurhash ? "blur" : "empty"}
+          blurDataURL={image.blurhash}
+        />
+      ) : (
+        <div
+          className={classNames(
+            "grid h-full w-full place-content-center bg-gray-300 dark:bg-gray-700",
+            className
+          )}
+        >
+          <BsImageAlt className="h-12 w-12" />
+        </div>
       )}
-      key={image.id}
-      src={src}
-      alt={image.id}
-      fill
-      quality={100}
-      unoptimized
-      placeholder={image.blurhash ? "blur" : "empty"}
-      blurDataURL={image.blurhash}
-    />
+    </>
   );
 };

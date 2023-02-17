6cd33348536a4e583cce5cc18c2fc34e89538025
ryohidaka <git@ryohidaka.jp>
2023-02-18 01:41:35 +0900

    feat: LPを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,24 @@
-import { TrendTags } from "@/components/Top/TrendTags";
-import { TopPosts } from "@/components/Top/TopPosts";
-import Link from "next/link";
-
-type Item = {
-  section: string;
-  contents: {
-    subSection: string;
-    url: string;
-    endpoint: string;
-    isGallery?: boolean;
-  }[];
-};
+import { LPHeader } from "@/components/LP/Header";
+import { LPContent } from "@/components/LP/Content";
+import { LPFeatures } from "@/components/LP/Features";
 
 /**
  * ホーム画面
  * @returns
  */
 export default function Home() {
-  const items: Item[] = [
-    {
-      section: "Gallery",
-      contents: [
-        {
-          subSection: "Posts",
-          url: "/gallery/posts",
-          endpoint: "/api/gallery/posts?limit=12",
-          isGallery: true,
-        },
-        {
-          subSection: "Featured Gallery",
-          url: "/gallery/featured",
-          endpoint: "/api/gallery/featured",
-          isGallery: true,
-        },
-        {
-          subSection: "Popular Gallery",
-          url: "/gallery/popular",
-          endpoint: "/api/gallery/popular",
-          isGallery: true,
-        },
-      ],
-    },
-    {
-      section: "Timeline",
-      contents: [
-        {
-          subSection: "Local Timeline",
-          url: "/notes/local",
-          endpoint: "/api/notes/local-timeline?limit=12",
-        },
-        {
-          subSection: "Hybrid Timeline",
-          url: "/notes/hybrid",
-          endpoint: "/api/notes/hybrid-timeline?limit=12",
-        },
-        {
-          subSection: "Global Timeline",
-          url: "/notes/global",
-          endpoint: "/api/notes/global-timeline?limit=12",
-        },
-      ],
-    },
-  ];
-
   return (
-    <>
-      {/* トレンドにあるハッシュタグ一覧 */}
-      <TrendTags />
+    <div className="h-full flex-grow pb-6 sm:pb-8 lg:pb-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        {/* ヘッダー */}
+        <LPHeader />
 
-      <div className="grid grid-cols-1 gap-10">
-        {items.map((item) => (
-          <section key={item.section} className="grid grid-cols-1 gap-6">
-            <h2 className="px-3 text-3xl tracking-tighter md:p-0">
-              {item.section}
-            </h2>
-            {item.contents.map((content) => (
-              <section
-                key={content.subSection}
-                className="grid grid-cols-1 gap-3"
-              >
-                <header className="flex items-center justify-between px-3 md:px-0">
-                  <h3 className="text-2xl tracking-tighter">
-                    {content.subSection}
-                  </h3>
-                  <Link href={content.url} className="hyperlink">
-                    more...
-                  </Link>
-                </header>
-                <TopPosts
-                  url={content.endpoint}
-                  isGallery={content.isGallery}
-                />
-              </section>
-            ))}
-          </section>
-        ))}
+        {/* メインコンテンツ */}
+        <LPContent />
+
+        {/* 機能説明 */}
+        <LPFeatures />
       </div>
-    </>
+    </div>
   );
 }
---
+++ src/components/LP/Content.tsx
@@ -0,0 +1,52 @@
+import { DESCRIPTION } from "@/modules/const";
+import Image from "next/image";
+import Link from "next/link";
+import { ExternalLink } from "../Common/ExternalLink";
+
+/**
+ * LPのコンテンツ
+ * @returns
+ */
+export const LPContent = () => {
+  return (
+    <section className="flex flex-col justify-between gap-6 sm:gap-10 md:gap-16 lg:flex-row">
+      <div className="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
+        <p className="mb-4 font-semibold text-indigo-500 md:mb-6 md:text-lg xl:text-xl">
+          Specialized in viewing illustrations
+        </p>
+
+        <h2 className="text-black-800 mb-8 text-4xl font-bold sm:text-5xl md:mb-12 md:text-6xl">
+          {DESCRIPTION}
+        </h2>
+
+        <p className="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg"></p>
+
+        <div className="flex flex-col gap-2.5 sm:flex-row sm:justify-center lg:justify-start">
+          <Link
+            href="/dashboard"
+            className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+          >
+            Start now
+          </Link>
+
+          <ExternalLink
+            href="https://misskey-hub.net/"
+            className="rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
+            showIcon
+          >
+            Misskey Hub
+          </ExternalLink>
+        </div>
+      </div>
+
+      <div className="overflow-hidden rounded-lg shadow-lg lg:h-auto xl:w-5/12">
+        <Image
+          src="/images/notes.png"
+          alt="notes"
+          fill
+          className="!relative block h-full w-full object-contain object-center"
+        />
+      </div>
+    </section>
+  );
+};

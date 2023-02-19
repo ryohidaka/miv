01e720910f07d8464ce1d0e5aeaecc5cb6b3a6d0
日高 凌 <git@ryohidaka.jp>
2023-02-20 02:24:05 +0900

    fix: ログインモーダルの背景色を修正

--- src/components/Auth/LoginModal.tsx
+++ src/components/Auth/LoginModal.tsx
@@ -13,7 +13,7 @@ export const LoginModal = () => {
     <>
       {showLoginModal && (
         <div className="fixed top-0 left-0 z-50 grid h-full w-full place-content-center">
-          <div className="relative z-50 h-full w-full max-w-2xl md:h-auto">
+          <div className="relative z-50 h-full w-full max-w-2xl md:h-auto bg-white dark:bg-gray-800">
             <LoginContent />
           </div>
 

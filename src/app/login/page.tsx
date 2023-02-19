08e582edda7a754a6274329f3c64b546da7c433d
ryohidaka <git@ryohidaka.jp>
2023-02-20 01:40:36 +0900

    feat: ログイン画面を追加

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginContent } from "@/components/Auth/LoginContent";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen w-screen">
+      <LoginContent />;
+    </div>
+  );
+}

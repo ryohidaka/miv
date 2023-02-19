263707c6cd1f3fa73dfbd8e791b231ee58b58e8c
ryohidaka <git@ryohidaka.jp>
2023-02-20 01:51:27 +0900

    feat: ログイン中の場合は、ログイン画面表示時にダッシュボード画面へリダイレクトする

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import { LoginContent } from "@/components/Auth/LoginContent";
+import { hasCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function LoginPage() {
+  const hasToken = hasCookie("token");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (hasToken) {
+      router.push("/dashboard");
+    }
+  }, []);
+
   return (
     <div className="h-screen w-screen">
       <LoginContent />;

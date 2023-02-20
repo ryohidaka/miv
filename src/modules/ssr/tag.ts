6d19701a64fd3c35f27fd256142c64dc7f5f68aa
ryohidaka <git@ryohidaka.jp>
2023-02-20 20:11:00 +0900

    feat: 使用率の高いハッシュタグの取得処理を追加

--- src/modules/ssr/tag.ts
+++ src/modules/ssr/tag.ts
@@ -10,3 +10,14 @@ export const getTrendTags = async () => {
   const tags: string[] = await res.json();
   return tags;
 };
+
+/**
+ * 使用率の高いハッシュタグ一覧
+ * @returns
+ */
+export const getFeaturedTags = async () => {
+  const path = `tags/list`;
+  const res = await fetchData(path);
+  const tags: string[] = await res.json();
+  return tags;
+};

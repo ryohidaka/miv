d4b45d408123eee24afadc6084f20f4d551d40bf
ryohidaka <git@ryohidaka.jp>
2023-02-22 18:01:06 +0900

    refactor: ログインフォームを別コンポーネントに分離

---
+++ src/components/Auth/LoginContent/LoginForm/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import { InstanceForm } from "./InstanceForm";
+import { InstanceSelector } from "./InstanceSelector";
+
+/**
+ * ログインフォーム
+ * @returns
+ */
+export const LoginForm = () => {
+  const [host, setHost] = useState<string>("misskey.io");
+  const loginUrl = `/api/auth/signin?host=${host}`;
+  const handleChange = (host: string) => {
+    setHost(host);
+  };
+
+  return (
+    <div id="signin" className="flex h-full w-full items-center justify-center">
+      <div className="selection:bg-indigo-500 selection:text-white">
+        <div className="flex items-center justify-center">
+          <div className="flex-1 p-8">
+            <div className="mx-auto overflow-hidden">
+              <div className="grid grid-cols-1 gap-10">
+                <h1 className="text-3xl font-bold text-indigo-600 md:text-5xl">
+                  Welcome back!
+                </h1>
+
+                {/* インスタンス一覧 */}
+                <InstanceSelector onChange={handleChange} />
+
+                {/* インスタンスURL入力欄 */}
+                <InstanceForm host={host} onChange={handleChange} />
+
+                {/* ログインボタン */}
+                <a href={loginUrl} className="light-btn text-center">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/Auth/LoginContent/index.tsx
+++ src/components/Auth/LoginContent/index.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import { ExternalLink } from "@/components/Common/ExternalLink";
 import classNames from "classnames";
-import { useState } from "react";
-import { InstanceForm } from "./InstanceForm";
-import { InstanceSelector } from "./InstanceSelector";
+
+import { LoginForm } from "./LoginForm";
 
 /**
  * ログイン
@@ -14,44 +11,10 @@ export const LoginContent = () => {
   const overlayBg =
     "bg-gradient-to-r from-blue-800 via-purple-800 to-indigo-800";
 
-  const [host, setHost] = useState<string>("misskey.io");
-  const loginUrl = `/api/auth/signin?host=${host}`;
-  const handleChange = (host: string) => {
-    setHost(host);
-  };
-
   return (
     <div className="grid h-screen w-screen grid-cols-1 text-black shadow dark:text-white md:grid-cols-2">
       {/* ログインフォーム */}
-      <div
-        id="signin"
-        className="flex h-full w-full items-center justify-center"
-      >
-        <div className="selection:bg-indigo-500 selection:text-white">
-          <div className="flex items-center justify-center">
-            <div className="flex-1 p-8">
-              <div className="mx-auto overflow-hidden">
-                <div className="grid grid-cols-1 gap-10">
-                  <h1 className="text-3xl font-bold text-indigo-600 md:text-5xl">
-                    Welcome back!
-                  </h1>
-
-                  {/* インスタンス一覧 */}
-                  <InstanceSelector onChange={handleChange} />
-
-                  {/* インスタンスURL入力欄 */}
-                  <InstanceForm host={host} onChange={handleChange} />
-
-                  {/* ログインボタン */}
-                  <a href={loginUrl} className="light-btn text-center">
-                    Login
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LoginForm />
 
       <div
         id="overlay-right"

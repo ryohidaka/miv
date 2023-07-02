be11765121b1f749f82e5aa8b36c9047f764c9e4
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-02 13:29:41 +0900

    feat: ログイン画面の多言語対応

--- src/components/Login/Tour.tsx
+++ src/components/Login/Tour.tsx
@@ -1,5 +1,6 @@
 import { ExternalLink } from "@/components/Common/ExternalLink";
 import classNames from "classnames";
+import useTranslation from "next-translate/useTranslation";
 
 /**
  * 新規ユーザ用案内
@@ -8,6 +9,8 @@ import classNames from "classnames";
 export const Tour = () => {
   const overlayBg =
     "bg-gradient-to-r from-blue-800 via-purple-800 to-indigo-800";
+
+  const { t } = useTranslation();
 
   return (
     <div
@@ -19,17 +22,19 @@ export const Tour = () => {
     >
       <div className="p-8 text-center">
         <h1 className="mb-4 text-4xl font-bold text-white md:text-6xl">
-          Don&apos;t have an account ?
+          {t("新規登録")}
         </h1>
 
-        <h5 className="text-xl text-white">Start your journey in one click</h5>
+        <h5 className="text-xl text-white">
+          {t("リンク先からご希望のインスタンスを検索してください")}
+        </h5>
         <div className="mt-16">
           <ExternalLink
             href="https://misskey-hub.net/instances.html"
             className="rounded-full bg-transparent py-3 px-6 text-center font-bold uppercase text-white hover:underline"
             showIcon
           >
-            List of instances
+            {t("インスタンス一覧")}
           </ExternalLink>
         </div>
       </div>

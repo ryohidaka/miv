3eca65ec5da46b66acacbafd2ab3bec4530fee98
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-18 08:53:03 +0900

    feat: 最近のギャラリー投稿画面のメタタグをMetadat APIに変更

--- src/app/(app)/gallery/posts/page.tsx
+++ src/app/(app)/gallery/posts/page.tsx
@@ -1,6 +1,12 @@
 import { Heading } from "@/components/Common/Heading";
 import { GalleryTimeline } from "@/components/Timeline/GalleryTimeline";
 
+const title = "最近の投稿";
+
+export const metadata = {
+  title,
+};
+
 /**
  * ギャラリー画面
  * @returns
@@ -8,7 +14,7 @@ import { GalleryTimeline } from "@/components/Timeline/GalleryTimeline";
 export default function GalleryPosts() {
   return (
     <>
-      <Heading text="最近の投稿" />
+      <Heading text={title} />
       <GalleryTimeline />
     </>
   );

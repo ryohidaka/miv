81458c200605d5570eacd6bb8a7b3d9e7440387e
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-18 08:25:44 +0900

    fix: 検索キーワード取得処理を修正

--- src/app/(app)/notes/search/page.tsx
+++ src/app/(app)/notes/search/page.tsx
@@ -1,13 +1,30 @@
+import { Heading } from "@/components/Common/Heading";
 import { SearchTimeline } from "@/components/Timeline/SearchTimeline";
 
-export const metadata = {
-  title: "検索",
+type Props = {
+  searchParams: {
+    q: string;
+  };
 };
+
+export async function generateMetadata({ searchParams }: Props) {
+  return { title: `検索: ${searchParams.q}` };
+}
 
 /**
  * ノート検索画面
  * @returns
  */
-export default function SearchNotes() {
-  return <SearchTimeline />;
+export default function SearchNotes({ searchParams }: Props) {
+  // 検索キーワードを取得
+  const query = searchParams.q;
+
+  const title = `検索: ${query}`;
+
+  return (
+    <>
+      <Heading text={title} />
+      <SearchTimeline query={query} />
+    </>
+  );
 }
--- src/components/Timeline/SearchTimeline.tsx
+++ src/components/Timeline/SearchTimeline.tsx
@@ -3,16 +3,16 @@
 import { usePostsWithPagination } from "@/hooks/post";
 import { Viewer } from "../Viewer";
 import { ViewerLayout } from "../Viewer/Layout";
-import { useQuery } from "@/hooks/search";
-import { Heading } from "../Common/Heading";
+
+type Props = {
+  query: string;
+};
 
 /**
  * 検索タイムライン
  * @returns
  */
-export const SearchTimeline = () => {
-  const query = useQuery();
-
+export const SearchTimeline = ({ query }: Props) => {
   const url = `/api/notes/search?q=${query}`;
 
   const mutate = query.length == 0;
@@ -26,11 +26,8 @@ export const SearchTimeline = () => {
     setSize(size + 1);
   };
 
-  const title = `検索: ${query}`;
-
   return (
     <ViewerLayout isLoading={isLoading} error={error}>
-      <Heading text={title} />
       <Viewer posts={data} hasMore={true} next={next} />
     </ViewerLayout>
   );

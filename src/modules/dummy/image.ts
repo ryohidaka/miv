ee52c5769c17ffa43cf5c044295a1781e65ad842
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-18 08:45:12 +0900

    remove: サンプル画面を削除

--- src/modules/dummy/image.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-import { Image } from "@/types/image";
-
-export function createRandomImage(): Image {
-  return {
-    id: faker.datatype.uuid(),
-    url: faker.image.cats(undefined, undefined, true),
-  };
-}

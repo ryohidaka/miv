6a2c29c196a1f728c4501cc018853a7434e98bde
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-07-18 08:51:23 +0900

    refact: 最近のギャラリー投稿画面をSSRに変更

--- src/app/(app)/gallery/posts/page.tsx
+++ src/app/(app)/gallery/posts/page.tsx
@@ -1,26 +1,15 @@
-"use client";
-
 import { Heading } from "@/components/Common/Heading";
-import { Viewer } from "@/components/Viewer";
-import { ViewerLayout } from "@/components/Viewer/Layout";
-import { usePostsWithPagination } from "@/hooks/post";
+import { GalleryTimeline } from "@/components/Timeline/GalleryTimeline";
 
 /**
  * ギャラリー画面
  * @returns
  */
 export default function GalleryPosts() {
-  const url = "/api/gallery/posts";
-  const { data, error, isLoading, size, setSize } = usePostsWithPagination(url);
-
-  const next = () => {
-    setSize(size + 1);
-  };
-
   return (
-    <ViewerLayout isLoading={isLoading} error={error}>
+    <>
       <Heading text="最近の投稿" />
-      <Viewer posts={data} hasMore={true} next={next} isGallery />
-    </ViewerLayout>
+      <GalleryTimeline />
+    </>
   );
 }
